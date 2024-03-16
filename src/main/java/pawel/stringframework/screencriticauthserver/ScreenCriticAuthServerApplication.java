e52680acf69cd85cd1fbbb8dbce3b32321abe29e
Pawel Styka <pawel@styka.pl>
2024-03-16 18:08:26 +0100

    first commit

---
+++ src/main/java/pawel/stringframework/screencriticauthserver/ScreenCriticAuthServerApplication.java
@@ -0,0 +1,13 @@
+package pawel.stringframework.screencriticauthserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScreenCriticAuthServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScreenCriticAuthServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pawel/stringframework/screencriticauthserver/ScreenCriticAuthServerApplicationTests.java
@@ -0,0 +1,13 @@
+package pawel.stringframework.screencriticauthserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScreenCriticAuthServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
